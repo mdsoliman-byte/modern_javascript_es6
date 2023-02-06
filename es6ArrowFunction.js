409a82516ed7eeb80c8a428443901be7377ec84d
mdsoliman-me <contact.mdsoliman@gmail.com>
2023-02-07 03:34:57 +0600

    function done

--- es6ArrowFunction.js
+++ es6ArrowFunction.js
@@ -26,12 +26,21 @@ const thanks = document.querySelector("#thanks")
 
 function showes() {
     display.innerHTML = this.value;
+    // if i use normal function than its dosnot work 
+    // ex: FUNCTION (){eturn}
     setTimeout(() => {
         thanks.innerHTML = `your Typing text ${this.value}`
     }, 1000)
 }
 
 
-
-
 searchInput.addEventListener("keyup", showes)
+
+// const Persion = (naem) => {
+//     this.naem = naem
+// }
+function Persion(name) {
+    this.name = name
+}
+const shakil = new Persion("Shakil")
+console.log(shakil)
